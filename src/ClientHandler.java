7a97b833a6915082f3a63d2f85d9702c4f3f0d14
Giacomo-Ambrogi <giacky597@gmail.com>
2025-12-12 13:17:23 +0100

    2

---
+++ src/ClientHandler.java
@@ -0,0 +1,14 @@
+import java.net.Socket;
+
+public class ClientHandler implements Runnable {
+    private Socket socket;
+
+    public ClientHandler(){
+
+    }
+
+    @Override
+    public void run(){
+
+    }
+}
---
+++ src/ClienteSemplice.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class ClienteSemplice {
+    public static void main(String[] args){
+        final String hostname = "127.0.0.1";
+        final int port = 12345;
+        try(Socket socket =new Socket(hostname, port)){
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.println("connesso al server, scrivi qualcosa");
+            String userInput;
+            while((userInput = scanner.nextLine()) != null) {
+                out.println(userInput);
+            }
+        }
+    }
+}
