cc5e5177f8fc1ec09261258f1e1f74d7a231b6fb
Giacomo-Ambrogi <giacky597@gmail.com>
2025-12-12 13:29:14 +0100

    2

--- src/ServerMultiThread.java
+++ src/ServerMultiThread.java
@@ -1,2 +1,11 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+
 public class ServerMultiThread {
+    public static void main(String[] args) throws IOException{
+        int port = 12345;
+        ServerSocket serverSocket = new ServerSocket(port);
+
+
+    }
 }
